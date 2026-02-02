02559728d4b9cad11f7dbca1e6f764188bbd8411
Pokey <parkerduthaler5@gmail.com>
2026-02-02 13:06:41 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 export default defineConfig({
+  server: {
+    allowedHosts: true as const,
+  },
   plugins: [react()],
   resolve: {
     alias: {
